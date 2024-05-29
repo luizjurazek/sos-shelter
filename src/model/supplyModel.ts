9d19926f05f8e2220d0630145c33e55a250ffe3d
Luiz Jurazek <luizjurazek@gmail.com>
2024-05-29 19:30:47 -0300

    fix: just insert autoincrement to SupplyModel

--- src/model/supplyModel.ts
+++ src/model/supplyModel.ts
@@ -9,6 +9,7 @@ const SupplyModel = sequelize.define(
       type: DataTypes.INTEGER,
       primaryKey: true,
       allowNull: false,
+      autoIncrement: true,
     },
     name: {
       type: DataTypes.STRING,
