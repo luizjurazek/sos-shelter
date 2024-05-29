b15b6245cadc105991962e266e2211708c37faaa
Luiz Jurazek <luizjurazek@gmail.com>
2024-05-29 19:15:38 -0300

    feat: create supply model

---
+++ src/model/supplyModel.ts
@@ -0,0 +1,46 @@
+import { DataTypes } from "sequelize";
+import { sequelize } from "../config/connection";
+import ShelterModel from "./shelterModel";
+
+const SupplyModel = sequelize.define(
+  "Supply",
+  {
+    id: {
+      type: DataTypes.INTEGER,
+      primaryKey: true,
+      allowNull: false,
+    },
+    name: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    quantity: {
+      type: DataTypes.DECIMAL(10, 2),
+      allowNull: false,
+      defaultValue: 0,
+    },
+    unit: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    id_shelter: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      references: {
+        model: "Shelter",
+        key: "id",
+      },
+    },
+  },
+  {
+    timestamps: true,
+    modelName: "Supply",
+    tableName: "supplies",
+  },
+);
+
+SupplyModel.belongsTo(ShelterModel, {
+  foreignKey: "id_shelter",
+});
+
+export default SupplyModel;
